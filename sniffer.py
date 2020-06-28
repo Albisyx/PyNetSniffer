5fbc5c35be3bacaa9dc719a9c97d435d6fe3a163
Albisyx <albywavelite@gmail.com>
2020-06-28 14:48:07 +0200

    Added get_interfaces()
    It performs some basic input validation

--- sniffer.py
+++ sniffer.py
@@ -10,9 +10,31 @@ def list_interfaces():
     print("Here's the available interfaces that i can listen to!")
     print("Please select one by typing the corresponding number:")
     interfaces = get_if_list()
+
     i = 1
     for item in interfaces:
         print("[{}] {}".format(i, item))
         i += 1
-    selection = int(input("Selected interface -> "))
+
+    selection = get_interface(len(interfaces))
     print(interfaces[selection-1])
+
+
+# Method for input validation
+def get_interface(bound):
+    while True:
+        # Check if the input value is a number
+        try:
+            value = int(input("Selected interface -> "))
+        except ValueError:
+            print("This is an invalid choice...")
+            continue
+
+        # Check if the input value is in the valid range
+        if value < 0 or value > bound:
+            print("Interface not in the list...")
+            continue
+        else:
+            break
+
+    return value
